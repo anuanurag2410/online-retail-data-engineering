c5fd3b257b4af4ee717e20422441af1d803fcadd
Anurag Srivastava <anuanurag2410@gmail.com>
2023-12-28 13:52:45 +0530

    Adding cosmos to run the DBT

--- dags/retail.py
+++ dags/retail.py
@@ -9,6 +9,10 @@ from astro import sql as aql
 from astro.files import File 
 from astro.constants import FileType 
 from astro.sql.table import Table,Metadata
+from include.dbt.cosmos_config import DBT_PROJECT_CONFIG,DBT_CONFIG
+from cosmos.airflow.task_group import DbtTaskGroup
+from cosmos.constants import LoadMode
+from cosmos.config import ProjectConfig, RenderConfig
 
 
 @dag(
@@ -57,5 +61,15 @@ def retail():
 
         return check(scan_name,checks_subpath)
     check_load()
+
+    transform = DbtTaskGroup(
+        group_id='transform',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/transform']
+        )
+    )
     
 retail()
